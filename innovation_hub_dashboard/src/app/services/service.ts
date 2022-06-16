47a394d686a7352e18cdc2d8384bcd79fd727fd9
Afaaq Suhail <afaqsuhail81@gmail.com>
2022-06-16 20:44:10 +0500

    Team Modal created with tabs

--- innovation_hub_dashboard/src/app/services/service.ts
+++ innovation_hub_dashboard/src/app/services/service.ts
@@ -6,84 +6,24 @@ import { HttpClient } from '@angular/common/http';
 
 export class GenericService {
 
+  baseUrl = 'http://3.128.28.79:4000/api';
+
   constructor(private http: HttpClient) {}
 
   getReleases() {
-    return this.http.get('')
+    return this.http.get(`http://3.128.28.79:7171/sheet-data/releases`)
   }
 
-  getTeamDetails(team: string) {
-     return {
-      name: 'Team 1',
-      roles: [
-        {
-          name: 'Backend',
-          resources: [
-            {
-              name: 'Asad',
-              isLead: true
-            },
-            {
-              name: 'Rujal',
-              isLead: false
-            }
-          ]
-        },
-        {
-          name: 'Frontend',
-          resources: [
-            {
-              name: 'Afaaq',
-              isLead: false
-            },
-            {
-              name: 'Gaurav',
-              isLead: false
-            }
-          ]
-        },
-        {
-          name: 'QA',
-          resources: [
-            {
-              name: 'Biru',
-              isLead: false
-            },
-            {
-              name: 'Ghulam',
-              isLead: false
-            },
-            {
-              name: 'Sahar',
-              isLead: false
-            }
-          ]
-        },
-        {
-          name: 'Product Owner',
-          resources: [
-            {
-              name: 'Muhammad Sajjid',
-              isLead: false
-            },
-          ]
-        },
-        {
-          name: 'Ops',
-          resources: [
-            {
-              name: 'Ahmad khan',
-              isLead: false
-            },
-          ]
-        },
-      ]
-    }
+  getTeamDetails() {
+    return this.http.get(`http://3.128.28.79:7171/sheet-data/teams`);
   }
-
 
   getTeams() {
-    return this.http.get('http://3.128.28.79:4000/api/issues');
+    return this.http.get(`http://3.128.28.79:4000/api/issues`);
+  }
+
+  getBacklog(team) {
+    return this.http.get(`http://3.128.28.79:4000/api/issues/backlog/`, { params: team })
   }
 
 }
